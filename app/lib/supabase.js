e6086f2a20aba18869c5a86d8c3595ddcb5b5892
harkion <gencfahrican@outlook.com>
2026-01-15 10:04:18 +0100

    supabase vercel fix .2

--- app/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://zfrgvmfuxhhxihbpdlal.supabase.co";
-const supabaseKey = "sb_publishable_Bd3qsA434WnVsG_4bhtZKQ_eHFV7Eqf";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
